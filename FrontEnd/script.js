70a69643803c61a3ded501eb5a88bf648f35bd57
Adrien Da Silva Ferreira <adrien@utopios.solutions>
2024-04-22 11:18:23 +0200

    Étape 1.2 : Réalisation du filtre des travaux

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,16 +1,53 @@
-const reponse = await (fetch(`http://localhost:5678/api/works/`));
-const works = await reponse.json();
+/* Récupération des tableaux */
+const works = await fetch(`http://localhost:5678/api/works/`).then(works => works.json());
+const categories = await fetch(`http://localhost:5678/api/categories/`).then(categories => categories.json());
+/* Récupération des tableaux */
 
+
+document.onload = genererworks(works)
+
+
+/* Fonction de génération des works */
+function genererworks(works){
 let figures = "";
 
 works.forEach(work => {
-    figures += `
-    <figure>
+    figures += `<figure>
         <img src="${work.imageUrl}" alt="${work.title}">
         <figcaption>${work.title}</figcaption>
-    </figure>
-    `
+    </figure>`
 });
 
-let body = document.querySelector(".gallery")
-body.innerHTML = figures
+let bodyFigures = document.querySelector(".gallery")
+bodyFigures.innerHTML = figures
+}
+/* Fonction de génération des works */
+
+
+/* Génération des boutons de filtres */
+let filters = "";
+
+filters += `<button class="filter-button">Tous</button>`
+categories.forEach(categories => {
+    filters += `<button class="filter-button">${categories.name}</button>`
+})
+let bodyCategories = document.querySelector(".filters")
+bodyCategories.innerHTML = filters
+/* Génération des boutons de filtres */
+
+/* Application des filtres */
+let filterButtons = document.querySelectorAll(".filter-button");
+    for(let i = 0; i < filterButtons.length; i++) {
+        filterButtons[i].addEventListener("click", function() {
+            const worksfiltres = works.filter(function (works) {
+                return works.categoryId == i;
+            });
+            if (i > 0){
+                document.querySelector(".gallery").innerHTML ="";
+                genererworks(worksfiltres)
+            } else {
+                document.querySelector(".gallery").innerHTML ="";
+                genererworks(works)   
+            }
+        })}
+/* Application des filtres */
